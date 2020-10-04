99f30d6eab8865fbe3f4080f3afa4fffb54da21a
Patrick Mattei <patrick.mattei@gmail.com>
2020-10-04 15:26:32 -0400

    Added avatar column for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,7 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :password, :email)
+      params.require(:user).permit(:username, :password, :email, :avatar)
     end
 
     def payload(id, username) 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_173338) do
+ActiveRecord::Schema.define(version: 2020_10_04_191457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_10_02_173338) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar"
   end
 
   add_foreign_key "games", "posts"
